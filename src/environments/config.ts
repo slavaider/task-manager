f9bf1756fe05d0810d00d55947376905255e1ce9
slavaider <slavaider1@gmail.com>
2022-05-09 16:01:37 +0500

    feat: add proxy

--- src/environments/config.ts
+++ src/environments/config.ts
@@ -1,1 +1,6 @@
-export const BASE_URL = 'http://localhost:4200';
+import { environment } from './environment';
+
+export const PROD_BACKEND_URL = 'http://v1115797.hosted-by-vdsina.ru:4000';
+export const DEV_BACKEND_URL = 'http://localhost:4200';
+
+export const BASE_URL = environment.production ? PROD_BACKEND_URL : DEV_BACKEND_URL;
