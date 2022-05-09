f28b604f686915b8a802426635099f86574c3b51
slavaider <slavaider1@gmail.com>
2022-05-09 15:43:27 +0500

    feat: add proxy

--- src/environments/config.ts
+++ src/environments/config.ts
@@ -1,7 +1,5 @@
-import { environment } from './environment';
+const getHost = () => {
+  return `${window.location.protocol}//${window.location.host}`;
+};
 
-export const PROD_BACKEND_URL =
-  'https://slavaider-proxy.herokuapp.com/http://v1115797.hosted-by-vdsina.ru:4000';
-export const DEV_BACKEND_URL = 'http://localhost:4200';
-
-export const BASE_URL = environment.production ? PROD_BACKEND_URL : DEV_BACKEND_URL;
+export const BASE_URL = getHost();
