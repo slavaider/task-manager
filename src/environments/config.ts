1da0ffed4d7b6c86ee18e2401c90be77713ebb3d
Vitali_Santalau <v.santalau@gmail.com>
2022-05-10 02:12:09 +0300

    change url

--- src/environments/config.ts
+++ src/environments/config.ts
@@ -1,6 +1,6 @@
 // export const BASE_URL = 'http://localhost:4200';
 
 
-// export const BASE_URL = 'https://task-manager-slavaider.vercel.app'
+export const BASE_URL = 'https://task-manager-slavaider.vercel.app'
 
-export const BASE_URL = 'https://task-manager-2y2rqkpy3-slavaider.vercel.app'
+// export const BASE_URL = 'https://task-manager-2y2rqkpy3-slavaider.vercel.app'
