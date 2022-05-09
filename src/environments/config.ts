447db54afe92d3d9c6a92e0cdf4144a04abb7007
slavaider <slavaider1@gmail.com>
2022-05-09 12:33:47 +0500

    feat: change env

--- src/environments/config.ts
+++ src/environments/config.ts
@@ -1,3 +1,5 @@
+import { isDevMode } from '@angular/core';
+
 export const BACKEND_URL = 'http://v1115797.hosted-by-vdsina.ru:4000/';
 
-export const BASE_URL = 'http://localhost:4200';
+export const BASE_URL = isDevMode() ? 'http://localhost:4200' : BACKEND_URL;
