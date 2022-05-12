5698d979a20d3dd7d9186631bd0a7f192865a996
anna-left <anna_left@mail.ru>
2022-05-12 14:49:34 +0300

    feat: install languagedetector

--- src/app/app-i18next.module.ts
+++ src/app/app-i18next.module.ts
@@ -1,36 +1,39 @@
 import { APP_INITIALIZER, LOCALE_ID, NgModule } from '@angular/core';
 import { I18NEXT_SERVICE, I18NextModule, ITranslationService, defaultInterpolationFormat } from 'angular-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
 
 export function appInit(i18next: ITranslationService) {
   return () =>
-    i18next.init({
-      supportedLngs: ['en', 'ru'],
-      fallbackLng: 'ru',
-      debug: true,
-      returnEmptyString: false,
-      ns: ['translation', 'validation', 'error'],
-      resources: {
-        en: {
-          translation: {
-            anykey: "en",
-            login: {
-              "signup": "en"
+    i18next
+      .use<any>(LanguageDetector)
+      .init({
+        supportedLngs: ['en', 'ru'],
+        fallbackLng: 'ru',
+        debug: true,
+        returnEmptyString: false,
+        ns: ['translation', 'validation', 'error'],
+        resources: {
+          en: {
+            translation: {
+              anykey: "en",
+              login: {
+                "signup": "en"
+              }
+            }
+          },
+          ru: {
+            translation: {
+              anykey: "ru",
+              login: {
+                "signup": "ru"
+              }
             }
           }
         },
-        ru: {
-          translation: {
-            anykey: "ru",
-            login: {
-              "signup": "ru"
-            }
-          }
+        interpolation: {
+          format: I18NextModule.interpolationFormat(defaultInterpolationFormat),
         }
-      },
-      interpolation: {
-        format: I18NextModule.interpolationFormat(defaultInterpolationFormat),
-      }
-    });
+      });
 }
 
 export function localeIdFactory(i18next: ITranslationService) {
