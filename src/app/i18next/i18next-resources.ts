cc336cf4e2afab1326412ffcffc01b28e940204e
anna-left <anna_left@mail.ru>
2022-05-12 20:34:11 +0300

    feat: localize login-page

--- src/app/i18next/i18next-resources.ts
+++ src/app/i18next/i18next-resources.ts
@@ -4,7 +4,18 @@ export const resources = {
               anykey: "en",
               header: {
                 "signIn": "Sign in",
-                "signup": "Sign up"
+                "signUp": "Sign Up",
+
+              },
+              loginPage: {
+                "signIn": "Sign in",
+                "Login": "Enter login",
+                "login": "enter login",
+                "Password": "Enter password",
+                "password": "enter password",
+                "Enter": "Sign in",
+                "notAccount": "Don't have an account?",
+                "Registration": "Sign Up",
               }
             }
           },
@@ -13,7 +24,18 @@ export const resources = {
               anykey: "ru",
               header: {
                 "signIn": "Войти",
-                "signup": "Регистрация"
+                "signUp": "Регистрация",
+
+              },
+              loginPage: {
+                "signIn": "Вход",
+                "Login": "Введите логин",
+                "login": "введите логин",
+                "Password": "Введите пароль",
+                "password": "введите пароль",
+                "Enter": "Вход",
+                "notAccount": "Нет аккаунта?",
+                "Registration": "Регистрация",
               }
             }
           }
