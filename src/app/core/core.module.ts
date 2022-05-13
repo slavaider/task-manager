8c16f998edcac8e92425de501d9e58af7662d6a8
anna-left <anna_left@mail.ru>
2022-05-13 07:59:09 +0300

    feat: localize confirm

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,10 +3,10 @@ import { CommonModule } from '@angular/common';
 import { ErrorsHandler } from './errors-handler/errors-handler';
 import { ConfirmComponent } from './components/confirm/confirm.component';
 import { MaterialModule } from '../material/material.module';
-
+import { AppI18nextModule } from './../app-i18next.module';
 @NgModule({
   declarations: [ConfirmComponent],
-  imports: [CommonModule, MaterialModule],
+  imports: [CommonModule, MaterialModule, AppI18nextModule],
   providers: [
     {
       provide: ErrorHandler,
--- src/app/i18next/i18next-resources.ts
+++ src/app/i18next/i18next-resources.ts
@@ -1,60 +1,70 @@
 export const resources = {
-          en: {
-            translation: {
-              anykey: "en",
-              header: {
-                "signIn": "Sign in",
-                "signUp": "Sign Up",
-              },
-              loginPage: {
-                "signIn": "Sign in",
-                "loginPlaceholder": "Enter login",
-                "loginMissing": "login cannot be empty",
-                "passwordPlaceholder": "Enter password",
-                "passwordMissing": "password cannot be empty",
-                "notAccount": "Don't have an account?",
-                "signUp": "Sign Up",
-              },
-              registerPage: {
-                "signUp": "Sign Up",
-                "namePlaceholder": "Enter name",
-                "nameMissing": "name cannot be empty",
-                "loginPlaceholder": "Enter login",
-                "loginMissing": "login cannot be empty",
-                "passwordPlaceholder": "Enter password",
-                "passwordMissing": "password cannot be empty",
-                "accountQuestion": "Have an account?",
-                "signIn": "Sign in",
-              }
-            }
-          },
-          ru: {
-            translation: {
-              anykey: "ru",
-              header: {
-                "signIn": "Войти",
-                "signUp": "Регистрация",
-              },
-              loginPage: {
-                "signIn": "Вход",
-                "loginPlaceholder": "Введите логин",
-                "loginMissing": "введите логин",
-                "passwordPlaceholder": "Введите пароль",
-                "passwordMissing": "введите пароль",
-                "notAccount": "Нет аккаунта?",
-                "signUp": "Регистрация",
-              },
-              registerPage: {
-                "signUp": "Регистрация",
-                "namePlaceholder": "Введите имя",
-                "nameMissing": "введите имя",
-                "loginPlaceholder": "Введите логин",
-                "loginMissing": "введите логин",
-                "passwordPlaceholder": "Введите пароль",
-                "passwordMissing": "введите пароль",
-                "accountQuestion": "Есть аккаунт?",
-                "signIn": "Вход",
-              }
-            }
-          }
-        };
+  en: {
+    translation: {
+      anykey: "en",
+      header: {
+        "signIn": "Sign in",
+        "signUp": "Sign Up",
+      },
+      loginPage: {
+        "signIn": "Sign in",
+        "loginPlaceholder": "Enter login",
+        "loginMissing": "login cannot be empty",
+        "passwordPlaceholder": "Enter password",
+        "passwordMissing": "password cannot be empty",
+        "notAccount": "Don't have an account?",
+        "signUp": "Sign Up",
+      },
+      registerPage: {
+        "signUp": "Sign Up",
+        "namePlaceholder": "Enter name",
+        "nameMissing": "name cannot be empty",
+        "loginPlaceholder": "Enter login",
+        "loginMissing": "login cannot be empty",
+        "passwordPlaceholder": "Enter password",
+        "passwordMissing": "password cannot be empty",
+        "accountQuestion": "Have an account?",
+        "signIn": "Sign in",
+      },
+      confirm: {
+        "confirmation": "Confirm action",
+        "yes": "Yes",
+        "no": "No"
+      }
+    }
+  },
+  ru: {
+    translation: {
+      anykey: "ru",
+      header: {
+        "signIn": "Войти",
+        "signUp": "Регистрация",
+      },
+      loginPage: {
+        "signIn": "Вход",
+        "loginPlaceholder": "Введите логин",
+        "loginMissing": "введите логин",
+        "passwordPlaceholder": "Введите пароль",
+        "passwordMissing": "введите пароль",
+        "notAccount": "Нет аккаунта?",
+        "signUp": "Регистрация",
+      },
+      registerPage: {
+        "signUp": "Регистрация",
+        "namePlaceholder": "Введите имя",
+        "nameMissing": "введите имя",
+        "loginPlaceholder": "Введите логин",
+        "loginMissing": "введите логин",
+        "passwordPlaceholder": "Введите пароль",
+        "passwordMissing": "введите пароль",
+        "accountQuestion": "Есть аккаунт?",
+        "signIn": "Вход",
+      },
+      confirm: {
+        "confirmation": "Подтвердите действие",
+        "yes": "Да",
+        "no": "Нет"
+      }
+    }
+  }
+};
